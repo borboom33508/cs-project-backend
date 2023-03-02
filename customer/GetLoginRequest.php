32b29d4c0e2a8e4429805fc0e2d1431e769cf3a6
Pheemaphat <Pheemaphat@gmail.com>
2023-03-02 22:08:15 +0700

    commit

---
+++ customer/GetLoginRequest.php
@@ -0,0 +1,29 @@
+<?php
+include "../DbConnect.php";
+
+$response = array();
+$request = array();
+
+$cus_email = $_GET['cus_email'];
+$cus_phone = $_GET['cus_phone'];
+$cus_password = $_GET['cus_password'];
+
+$sql = "SELECT * FROM customer WHERE (cus_email = '" . $cus_email . "' || cus_phone = '" . $cus_phone . "') 
+&& cus_password = '" . $cus_password . "'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while ($row = mysqli_fetch_assoc($result)) {
+        $request = array('cus_id' => $row["cus_id"]);
+    }
+    $response['success'] = true;
+    $response['request'] = $request;
+} else {
+    $response['success'] = false;
+}
+
+echo json_encode($response);
+mysqli_close($conn);
+?>
