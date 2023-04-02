f3d209f0d43b93d8e5b97b30105f1cc883f37315
borbyi <boraumor499@gmail.com>
2023-04-02 17:40:25 +0700

    update

--- laundry/GetLaundryData.php
+++ laundry/GetLaundryData.php
@@ -8,7 +8,7 @@ $request = array();
 
 $laundry_id = $_GET['laundry_id'];
 
-$sql = "SELECT laundry_name, laundry_email, laundry_phone, laundry_picture, laundry_hours, laundry_rating, laundry_location  FROM laundry WHERE laundry_id = '" . $laundry_id . "' ";
+$sql = "SELECT laundry_name, laundry_email, laundry_phone, laundry_picture, laundry_hours, laundry_rating, laundry_location, laundry_ownerFname, laundry_ownerLname, laundry_credit  FROM laundry WHERE laundry_id = '" . $laundry_id . "' ";
 
 $result = mysqli_query($conn, $sql);
 
@@ -16,7 +16,8 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while ($row = mysqli_fetch_assoc($result)) {
         $request = array('laundry_name' => $row["laundry_name"], 'laundry_email' => $row["laundry_email"], 'laundry_phone' => $row["laundry_phone"], 
-        'laundry_picture' => $row['laundry_picture'], 'laundry_hours' => $row["laundry_hours"], 'laundry_rating' => $row["laundry_rating"], 'laundry_location' => $row["laundry_location"]);
+        'laundry_picture' => $row['laundry_picture'], 'laundry_hours' => $row["laundry_hours"], 'laundry_rating' => $row["laundry_rating"], 'laundry_location' => $row["laundry_location"],
+        'laundry_ownerFname' => $row["laundry_ownerFname"], 'laundry_ownerLname' => $row["laundry_ownerLname"], 'laundry_credit' => $row["laundry_credit"]);
     }
     $response['success'] = true;
     $response['request'] = $request;
--- laundry/GetWithdrawLaundry.php
+++ laundry/GetWithdrawLaundry.php
@@ -8,7 +8,7 @@ $request = array();
 
 $laundry_id = $_GET['laundry_id'];
 
-$sql = "SELECT laundry_credit, laundry_accountNumber, laundry_bankName, laundry_ownerFname, laundry_ownerLname  FROM laundry WHERE laundry_id = '" . $laundry_id . "' ";
+$sql = "SELECT laundry_credit, laundry_accountNumber, laundry_bankName, laundry_ownerFname, laundry_ownerLname, laundry_phone  FROM laundry WHERE laundry_id = '" . $laundry_id . "' ";
 
 $result = mysqli_query($conn, $sql);
 
@@ -16,7 +16,7 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while ($row = mysqli_fetch_assoc($result)) {
         $request = array('laundry_credit' => $row["laundry_credit"], 'laundry_accountNumber' => $row["laundry_accountNumber"], 'laundry_bankName' => $row["laundry_bankName"], 
-        'laundry_ownerFname' => $row['laundry_ownerFname'], 'laundry_ownerLname' => $row["laundry_ownerLname"]);
+        'laundry_ownerFname' => $row['laundry_ownerFname'], 'laundry_ownerLname' => $row["laundry_ownerLname"], 'laundry_phone' => $row["laundry_phone"]);
     }
     $response['success'] = true;
     $response['request'] = $request;
